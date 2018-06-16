3be506525c80fcc1ab5d0d381888dd4ef6d5d044
GlebKalinichenko <gleb.kalinichenko@umbrella-web.com>
2018-06-16 22:47:13 +0300

    Configurate project

---
+++ app/src/main/java/codelab/android/library/fragmentmanager/MainActivity.kt
@@ -0,0 +1,12 @@
+package codelab.android.library.fragmentmanager
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
